93f1e7b4fecf5899dbdfae424ad99828aa3531ee
Matheus Henrique <teus.henrique021@gmail.com>
2026-02-15 19:16:57 -0300

    Menu interativo

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("\n---MENU---");
+            System.out.println("1 - Adicionar contato");
+            System.out.println("2 - Listar contatos");
+            System.out.println("3 - Editar contato");
+            System.out.println("4 - Deletar contato");
+            System.out.println("0 - Sair");
+
+            System.out.print("\nDigite qual item deseja utilizar: ");
+            int opcao = sc.nextInt();
+
+            if (opcao == 0){
+                break;
+            }
+        }
+    }
+}
